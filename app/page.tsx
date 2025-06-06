b52b00339bdd5661fdfda3283a937716256120e8
dipu59 <12biswasdipu@gmail.com>
2025-06-06 23:06:22 +0600

    Invironment Setup for my portfolio

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <h1 className='text-center text-5xl m-4 text-gray-800 font-bold'>Hello, Portfolio....!</h1>
+    </div>
+  )
+}
+
+export default page
